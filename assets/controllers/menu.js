b5248db6672cb9b4a143db17b7b6288bd18a5024
Ben <ben.crommen@gmail.com>
2021-05-20 22:19:55 +0200

    burger effect + menu link + social media navlinks

--- assets/controllers/menu.js
+++ assets/controllers/menu.js
@@ -3,3 +3,9 @@ const menuBurgerBtn = document.querySelector('.burger');
 menuBurgerBtn.addEventListener('click', () => {
     document.body.classList.toggle('menu-open');
 })
+
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+})
